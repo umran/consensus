67fb0487f8128f2f2b38158e07165f98d4d5594b
Umran Hussein <umran.hussein@gmail.com>
2018-08-26 03:17:24 -0700

    aaaaarghh

--- src/utilities/encoders.js
+++ src/utilities/encoders.js
@@ -1,30 +1,49 @@
-require("google-closure-library")
-goog.require("goog.crypt.base64")
+// require("google-closure-library")
+// goog.require("goog.crypt.base64")
+//
+// exports.byteArrayToB64 = function(bytes) {
+// 	return goog.crypt.base64.encodeByteArray(bytes)
+// }
+//
+// exports.b64ToByteArray = function(b64) {
+// 	return goog.crypt.base64.decodeStringToUint8Array(b64)
+// }
+//
+// exports.stringToB64 = function(string) {
+// 	var b = new Buffer(string)
+// 	return b.toString('base64')
+// }
+//
+// exports.b64ToString = function(b64) {
+// 	var b = new Buffer(b64, 'base64')
+// 	return b.toString()
+// }
+//
+// exports.stringToByteArray = function(string) {
+// 	string = this.stringToB64(string)
+// 	return this.b64ToByteArray(string)
+// }
+//
+// exports.byteArrayToString = function(bytes) {
+// 	bytes = this.byteArrayToB64(bytes)
+// 	return this.b64ToString(bytes)
+// }
 
-exports.byteArrayToB64 = function(bytes) {
-	return goog.crypt.base64.encodeByteArray(bytes)
-}
+
+var util = require('tweetnacl-util');
 
 exports.b64ToByteArray = function(b64) {
-	return goog.crypt.base64.decodeStringToUint8Array(b64)
+	return util.decodeBase64(b64)
 }
 
-exports.stringToB64 = function(string) {
-	var b = new Buffer(string)
-	return b.toString('base64')
-}
-
-exports.b64ToString = function(b64) {
-	var b = new Buffer(b64, 'base64')
-	return b.toString()
+exports.byteArrayToB64 = function(bytes) {
+	return util.encodeBase64(bytes)
 }
 
 exports.stringToByteArray = function(string) {
-	string = this.stringToB64(string)
-	return this.b64ToByteArray(string)
+	return util.decodeUTF8(string)
 }
 
 exports.byteArrayToString = function(bytes) {
-	bytes = this.byteArrayToB64(bytes)
-	return this.b64ToString(bytes)
+	return util.encodeUTF8(bytes)
 }
